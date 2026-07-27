c0a1ea32f2efe2544c6b0323d9de4ad657ef34df
jangap411 <angapj@gmail.com>
2026-07-27 23:36:47 +1000

    Added boilerplate code

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+// health check route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// api routes
+
+//404 handler
+app.all(/.*/, (req, res) => {
+  res.status(404).json({
+    message: "404 not found",
+  });
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  console.log("\n\n\nError handler middleware called\n\n\n");
+  console.error(err.stack);
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
+
+//export the app
+module.exports = app;
---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const http = require("http");
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
+
+// create http server
+const server = http.createServer(app);
+
+// start the server
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
